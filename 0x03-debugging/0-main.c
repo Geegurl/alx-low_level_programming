1336d1e90b1bd865c82e930546372bb9d0f69af7
Golda  Ekene <keegana007@gmail.com>
2022-12-14 14:27:05 -0800

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
-**/
+*/
 
 int main(void)
 {
@@ -13,4 +13,5 @@ i = 0
 positive_or_negative(i);
 
 return (0);
+
 }
