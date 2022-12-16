d48ac94fdbc46e6fd476df7f291d7ef32886e9d7
Golda  Ekene <keegana007@gmail.com>
2022-12-16 15:43:58 -0800

    Variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,12 @@ int main(void)
 	char c = 'a';
 	int num;
 
-	for (num = 0; num < 16; num ++)
+	for (num = 0; num < 16; num++)
 	{
 		if (num < 10)
 		{
 			putchar(num + '0');
-		}else
+		} else
 		{
 			putchar(c);
 			c++;
