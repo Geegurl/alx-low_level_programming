2dee6f676427ac2806efef641dc8ca3fe427277c
Golda  Ekene <keegana007@gmail.com>
2023-01-03 13:05:04 -0800

    pointer seven

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,12 +7,13 @@
  *
  * Return: copied memory n with byted changed
  */
-char *memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
 
-	for(; r < i; r++)
+
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
